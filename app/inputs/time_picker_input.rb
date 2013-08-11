14be0d5ec6f528fb0a51b49d578b15f3e10bd3da
johnrees <john@bitsushi.com>
2013-08-11 21:50:56 +0200

    removed simple form input

--- app/inputs/time_picker_input.rb
+++
@@ -1,7 +0,0 @@
-class TimePickerInput < SimpleForm::Inputs::FileInput
-  def input
-    out = '' # the output string we're going to build
-    # check if there's an uploaded file (eg: edit mode or form not saved)
-    (out << @builder.file_field(attribute_name, input_html_options)).html_safe
-  end
-end
