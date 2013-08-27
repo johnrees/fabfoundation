82a126e6e65f0399261617f0985c0f7ce996fedc
John Rees <john@bitsushi.com>
2013-08-27 10:04:00 +0900

    seeds

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Fabfoundation::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { host: 'http://fabfoundation.johnre.es' }
+  config.action_mailer.default_url_options = { host: 'http://labs.fabfoundation.org' }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,48 +3,48 @@
 #   ToolType.create!(name: type.humanize.capitalize, slug: type)
 # end
 
-User.delete_all
-User.create!(first_name: 'John', last_name: 'Rees', admin: true, email: 'admin@bitsushi.com', password: 'password')
-User.create!(first_name: 'John', last_name: 'Rees', email: 'john@bitsushi.com', password: 'password')
+# User.delete_all
+# User.create!(first_name: 'John', last_name: 'Rees', admin: true, email: 'admin@bitsushi.com', password: 'password')
+# User.create!(first_name: 'John', last_name: 'Rees', email: 'john@bitsushi.com', password: 'password')
 
-# Lab.delete_all
-# Lab.after_save.clear
-# # Lab.skip_callback(:create)
-# labs = JSON.parse( File.read('public/oldlabs.json') )
-# labs.each do |lab|
-#   if lab['address'].present?
-#     begin
+Lab.delete_all
+Lab.after_save.clear
+# Lab.skip_callback(:create)
+labs = JSON.parse( File.read('public/oldlabs.json') )
+labs.each do |lab|
+  if lab['address'].present?
+    begin
 
-#       l = Lab.create!(
-#         name: lab['name'],
-#         street_address_1: lab['address']['building'],
-#         city: lab['address']['city'],
-#         country_code: lab['address']['country'].downcase,
-#         latitude: lab['address']['latitude'],
-#         longitude: lab['address']['longitude'],
-#         urls: (lab['websites'].map{|m| m['url']}.join("\n") if lab['websites']),
-#         kind: Lab::Kinds.index(lab['kind'] == "fab_lab" ? lab['kind'] : "#{lab['kind']}_fab_lab"),
-#         state: :approved
-#         # humans: humans
-#       )
+      l = Lab.create!(
+        name: lab['name'],
+        street_address_1: lab['address']['building'],
+        city: lab['address']['city'],
+        country_code: lab['address']['country'].downcase,
+        latitude: lab['address']['latitude'],
+        longitude: lab['address']['longitude'],
+        urls: (lab['websites'].map{|m| m['url']}.join("\n") if lab['websites']),
+        kind: Lab::Kinds.index(lab['kind'] == "fab_lab" ? lab['kind'] : "#{lab['kind']}_fab_lab"),
+        state: :approved
+        # humans: humans
+      )
 
-#       lab['contacts'].each do |contact|
-#         h = l.humans.create!(
-#           email: contact['email'],
-#           full_name: contact['name'],
-#           phone: contact['phone'])
-#         # p h
-#       end
-#       # lab.save
+      lab['contacts'].each do |contact|
+        h = l.humans.create!(
+          email: contact['email'],
+          full_name: contact['name'],
+          phone: contact['phone'])
+        # p h
+      end
+      # lab.save
 
-#       p "added  - #{lab['name']}"
-#     rescue
-#       p "fail   - #{lab['name']}"
-#     end
-#   end
-# end
-
-Facility.delete_all
-['Laser Engraving', '3D Printing', 'CNC Milling', 'Precision Milling', 'Vinyl Cutting', 'Electronic Circuit Production'].each do |type|
-  Facility.create!(name: type, slug: type.parameterize)
+      p "added  - #{lab['name']}"
+    rescue
+      p "fail   - #{lab['name']}"
+    end
+  end
 end
+
+# Facility.delete_all
+# ['Laser Engraving', '3D Printing', 'CNC Milling', 'Precision Milling', 'Vinyl Cutting', 'Electronic Circuit Production'].each do |type|
+#   Facility.create!(name: type, slug: type.parameterize)
+# end
