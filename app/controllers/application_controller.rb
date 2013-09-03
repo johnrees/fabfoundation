e7b6eaf41fc23c3bd21adf8eaa64a8dfebc8fbbe
John Rees <john@bitsushi.com>
2013-09-03 14:54:15 +0900

    removed timezone and tidied application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,11 @@
 class ApplicationController < ActionController::Base
 
-  # around_filter :user_time_zone, if: :current_user
-
   # CanCan workaround
   # See: https://github.com/ryanb/cancan/issues/835#issuecomment-18663815
   before_filter do
     resource = controller_name.singularize.to_sym
     method = "#{resource}_params"
     params[resource] &&= send(method) if respond_to?(method, true)
-  end
-
-  protect_from_forgery with: :exception
-  def not_authenticated
-    redirect_to signin_url, :alert => "Oops. You'll need to sign in to do that."
   end
 
   def require_login
@@ -37,14 +30,9 @@ private
     end
   end
 
-
   def body_classes
     "c-#{controller_name} a-#{action_name}"
   end
-
-  # def user_time_zone(&block)
-  #   Time.use_zone(current_user.time_zone, &block)
-  # end
 
   helper_method :current_user
   helper_method :body_classes
