149ac573b271bd03847ff2f807cfaa6d0c755103
John Rees <john@bitsushi.com>
2013-10-09 11:45:34 +0200

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Fabfoundation::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( backstage.css forms.js )
+  config.assets.precompile += %w( backstage.js backstage.css forms.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
